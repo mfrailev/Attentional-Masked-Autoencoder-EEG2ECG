74681d0046cc8e176e262310c55079ca73d3975c
MfraileV <matiasfraile95@gmail.com>
2026-03-27 17:53:35 -0500

    VSCODE

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector <string> msg{"Hello", "C++", "Wold"};
+    for (const string &word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
