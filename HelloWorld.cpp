c6b00e68b68f7bf96af590e01c517ec7ab27c77a
MfraileV <matiasfraile95@gmail.com>
2026-03-28 21:45:29 -0500

    changed name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector <string> msg{"Hello", "C++", "Wold"};
+    vector <string> msg{"Hello", "Wold"};
     for (const string &word : msg)
     {
         cout << word << " ";
