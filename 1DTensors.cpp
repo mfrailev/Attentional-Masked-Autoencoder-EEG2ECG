c2500ab635656d93bbf6f4f52ff928cca5c15233
MfraileV <matiasfraile95@gmail.com>
2026-04-02 16:58:10 -0500

    Finished matmul

--- 1DTensors.cpp
+++ 1DTensors.cpp
@@ -16,11 +16,11 @@ class Tensor{
             assert(r >= 0 && r <= rows && "Row index out of Bounds!");
             assert(c >= 0 && c <= cols && "Column index out of Bounds!");
             return data[r * cols + c];
-        };
+        }
 
         void fill_constant(float value){
             std::fill(data.get(),data.get() + (rows * cols), value);
-        };
+        }
 
         void fill_random(){
             std::default_random_engine generator;
@@ -28,11 +28,10 @@ class Tensor{
 
             for (int i = 0; i < (rows*cols); i++){
                 data[i] = distribution(generator);
-            };
-        };
+            }
+        }
 
         void transpose(Tensor& ws){
-
             assert(ws.cols == rows && ws.rows == cols && "Matrix size is not compatible with tensor transpose!");
 
             for (int i = 0; i < rows; i++){
@@ -41,16 +40,37 @@ class Tensor{
                 }
             }
         }
+
+        void matmul(Tensor& multi, Tensor& output){
+            assert(this->cols == multi.rows && "Rows and columns must match!");
+            assert(output.rows == this->rows && output.cols == multi.cols && "Output tensor is the wrong shape!");
+
+            output.fill_constant(0.0f);
+
+            for(int i = 0; i < this->rows; i++){
+                for (int j = 0; j < multi.cols; j++){
+                    for (int k = 0; k < this->cols; k++){
+                    output(i,j) += (*this)(i,k) * multi(k,j);
+                    }
+                }
+            }
+        }
 };
 
 int main(){
     Tensor eeg_window(1024,129);
     Tensor workspace(129,1024); // Transpose pre-allocation
+    Tensor matmulMat(1024,1024);
+    Tensor outputMat(129,1024);
 
     eeg_window(0,128) = 0.5f;
     eeg_window.fill_random();
+    matmulMat.fill_random();
+
     std::cout << "Value at (0, 128): " << eeg_window(64, 128) << std::endl;
     eeg_window.transpose(workspace);
-    std::cout << "Value at (0, 128): " << eeg_window(64, 128) << std::endl;
+    std::cout << "Value at (0, 128): " << workspace(128, 64) << std::endl;
+    workspace.matmul(matmulMat, outputMat);
+    std::cout << "Value at (0, 128): " << outputMat(128, 34) << std::endl;
     return 0;
 }
